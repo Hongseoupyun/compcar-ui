7d2de7f80803397b7c8f21342240fa2d9f8381ed
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-11-04 12:24:32 -0400

    installed sass and added basic html skeleton

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import "./Navbar.scss"
-
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <img className='logo' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/logo.png')} alt="logo" />
+      <div className='content-wrapper'>
+        <div className='find-a-car'>
+          Find a car
+        </div>
+        <div className='deals'>
+          Deals
+        </div>
+        <div className='contact'>
+          Contact
+        </div>
+        <button className='sign-in'>
+          Sign in
+        </button>
+      </div>
+    </div>
   )
 }
