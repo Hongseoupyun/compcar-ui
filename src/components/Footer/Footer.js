306786816078dc63563c649a51cea447d1f4c922
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-11-05 23:24:46 -0400

    added basic skeleton for Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <div>Compare and buy reasonable car from anywhere to everywhere</div>
+      <div>ⓒ 2022 CompCar Corporation. All rights reserved.</div>
+    </div>
+  );
+}
