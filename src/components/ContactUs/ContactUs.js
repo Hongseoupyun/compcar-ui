ab16dda5f7ef71b6084cb68f33136d102ce1954c
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-11-09 23:54:53 -0500

    developed(ContactUs):added footer components in ContactUs component

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,24 +1,28 @@
 import React from 'react'
+import Footer from '../Footer/Footer'
 import "./ContactUs.scss"
 export default function ContactUs() {
   return (
-    <div className='contact-us'>
-      <div className='meet-our-team'>
-        MEET OUR TEAM
+    <>
+      <div className='contact-us'>
+        <div className='meet-our-team'>
+          MEET OUR TEAM
+        </div>
+        <div className='members-img-container'>
+          <img id='contact-us-img' alt='sehwan' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/Sehwan.jpg')}>
+          </img>
+          <img id='contact-us-img' alt='anna' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/anna.jpg')}>
+          </img>
+          <img id='contact-us-img' alt='hongseoup' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/simon.jpg')}>
+          </img>
+          <img id='contact-us-img' alt='jay' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/Jay.jpg')}>
+          </img>
+        </div>
+        <button className='contact-button'>
+          Contact Us
+        </button>
       </div>
-      <div className='members-img-container'>
-        <img id='contact-us-img' alt='sehwan' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/Sehwan.jpg')}>
-        </img>
-        <img id='contact-us-img' alt='anna' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/anna.jpg')}>
-        </img>
-        <img id='contact-us-img' alt='hongseoup' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/simon.jpg')}>
-        </img>
-        <img id='contact-us-img' alt='jay' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/Jay.jpg')}>
-        </img>
-      </div>
-      <button className='contact-button'>
-        Contact Us
-      </button>
-    </div>
+      <Footer />
+    </>
   )
 }
