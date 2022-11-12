371e8ffdae18e341e98705393d06d3a1adbee047
ashin526 <anna.shin526@gmail.com>
2022-11-11 22:21:33 -0500

    develop(Card):develop Card components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
+import Card from './components/Card/Card';
+import PopularCard from './components/Card/PopularCard';
+import Button from './components/Card/Button';
+import data from './data';
+import cityData from './cityData';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <div className="title">
+        <h2 className="sub-title">The best deals</h2>
+        <div className="sub-explain">Buy the car with the best deal we’ve found this week. </div>
+        <div className="more-option">see more deals</div>
+      </div>
+      <div className="deal">
+        {data.map (data=> (
+        <Card 
+          image={data.image}
+          brand={data.brand}
+          model={data.model}
+          price={data.price}
+          mileage={data.mileage}
+        />
+          ))}
+      </div>
+      
+      <div className="title">
+        <h2 className="sub-title">Most Popular Cars in Canada</h2>
+        <Button/>
+        <div className="more-option">View more cities</div>
+      </div>
+      <div className="deal">
+        {cityData.map (cityData=> (
+        <PopularCard 
+          image={cityData.image}
+          cityName={cityData.cityName}
+          model1={cityData.model1}
+          model2={cityData.model2}
+          model3={cityData.model3}
+        />
+          ))}
+      </div>
       <Footer/>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,27 @@
+const carCard = [
+    {
+        id: 1,
+        brand: "Honda",
+        image: "https://www.cnet.com/a/img/resize/1e024a01ae63e402e22da463926bc11f04a1a909/hub/2021/06/25/ef1fc0ac-d839-4579-a846-8759f5cdafc0/2022-honda-civic-sport-sedan-007.jpg?auto=webp&width=1920",
+        model: "civic",
+        price: 10000,
+        mileage: 23456
+    },
+    {
+        id: 2,
+        brand: "Hyundai",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Genesis_GV80_IAA_2021_1X7A0230.jpg/1920px-Genesis_GV80_IAA_2021_1X7A0230.jpg",
+        model: "Genecis GV80",
+        price: 20000,
+        mileage: 12345
+    },
+    {
+        id: 3,
+        brand: "BMW",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/2019_BMW_X5_M50d_Automatic_3.0.jpg/1280px-2019_BMW_X5_M50d_Automatic_3.0.jpg",
+        model: "X5",
+        price: 15000,
+        mileage: 112345
+    }
+];
+export default carCard;
