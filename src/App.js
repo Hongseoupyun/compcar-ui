3a7f11a4e0f252e5eb68b3e0cebcf672a942d131
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-11-16 23:01:04 -0500

    develope(ContactUs):basic layout of ContactUs page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import ContactUs from './components/ContactUs/ContactUs';
-import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,28 +1,68 @@
-import React from 'react'
-import Footer from '../Footer/Footer'
-import "./ContactUs.scss"
+import React from "react";
+import Footer from "../Footer/Footer";
+import "./ContactUs.scss";
 export default function ContactUs() {
   return (
     <>
-      <div className='contact-us'>
-        <div className='meet-our-team'>
-          MEET OUR TEAM
+      <div className="contact-us">
+        <div className="meet-our-team">MEET OUR TEAM</div>
+        <div className="members-img-container">
+          <div className="members-img-wrapper">
+            <img
+              id="contact-us-img"
+              alt="sehwan"
+              src={require("/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/Sehwan.jpg")}
+            />
+            <div className="name">
+              <b>Sehwan Oh</b>
+            </div>
+            <div className="role">
+              <i>Lead Developer</i>
+            </div>
+          </div>
+          <div className="members-img-wrapper">
+            <img
+              id="contact-us-img"
+              alt="anna"
+              src={require("/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/anna.jpg")}
+            />
+            <div className="name">
+              <b>Anna Shin</b>
+            </div>
+            <div className="role">
+              <i>FE/UI Developer</i>
+            </div>
+          </div>
+          <div className="members-img-wrapper">
+            <img
+              id="contact-us-img"
+              alt="hongseoup"
+              src={require("/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/simon.jpg")}
+            />
+            <div className="name">
+              <b>Simon Yun</b>
+            </div>
+            <div className="role">
+              <i>FE/UI Developer</i>
+            </div>
+          </div>
+          <div className="members-img-wrapper">
+            <img
+              id="contact-us-img"
+              alt="jay"
+              src={require("/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/Jay.jpg")}
+            />
+            <div className="name">
+              <b>Jay Choi</b>
+            </div>
+            <div className="role">
+              <i>BE Developer</i>
+            </div>
+          </div>
         </div>
-        <div className='members-img-container'>
-          <img id='contact-us-img' alt='sehwan' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/Sehwan.jpg')}>
-          </img>
-          <img id='contact-us-img' alt='anna' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/anna.jpg')}>
-          </img>
-          <img id='contact-us-img' alt='hongseoup' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/simon.jpg')}>
-          </img>
-          <img id='contact-us-img' alt='jay' src={require('/Users/hongseoupyun/Desktop/hongseoup/sideProjects/compcar-ui/src/assets/Jay.jpg')}>
-          </img>
-        </div>
-        <button className='contact-button'>
-          Contact Us
-        </button>
+        <button className="contact-button">Contact Us</button>
       </div>
       <Footer />
     </>
-  )
+  );
 }
