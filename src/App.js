039c3fe5ee44a8840b4c5bf27be279ea5ee214c7
ashin526 <anna.shin526@gmail.com>
2022-11-18 15:28:47 -0500

    Fixed App.js and Create Cardapp.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
-import Card from './components/Card/Card';
-import PopularCard from './components/Card/PopularCard';
-import Button from './components/Card/Button';
-import data from './data';
-import cityData from './cityData';
+import Cardapp from './components/Card/Cardapp';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <p>hello</p>
-      <div className="title">
-        <h2 className="sub-title">The best deals</h2>
-        <div className="sub-explain">Buy the car with the best deal we’ve found this week. </div>
-        <div className="more-option">see more deals</div>
-      </div>
-      <div className="deal">
-        {data.map (data=> (
-        <Card 
-          image={data.image}
-          brand={data.brand}
-          model={data.model}
-          price={data.price}
-          mileage={data.mileage}
-        />
-          ))}
-      </div>
-      
-      <div className="title">
-        <h2 className="sub-title">Most Popular Cars in Canada</h2>
-        <Button/>
-        <div className="more-option">View more cities</div>
-      </div>
-      <div className="deal">
-        {cityData.map (cityData=> (
-        <PopularCard 
-          image={cityData.image}
-          cityName={cityData.cityName}
-          model1={cityData.model1}
-          model2={cityData.model2}
-          model3={cityData.model3}
-        />
-          ))}
-      </div>
+      <Cardapp/>
       <Footer/>
     </div>
   );
---
+++ src/components/Card/Cardapp.js
@@ -0,0 +1,47 @@
+import './Cardapp.css';
+import Card from './Card';
+import PopularCard from './PopularCard';
+import Button from './Button';
+import data from './data';
+import cityData from './cityData';
+
+function Cardapp(){
+    return (
+        <div className="cardApp">
+            <div className ="title">
+                <h2 className = "sub-title">The best deals</h2>
+                <div className = "sub-explain">Buy the car with the best deal we've found this week. </div>
+                <div className = "more-option">see more deals</div>
+            </div>
+            <div className ="deal">
+                {data.map (data=> (
+                    <Card 
+                    image={data.image}
+                    brand={data.brand}
+                    model={data.model}
+                    price={data.price}
+                    mileage={data.mileage}
+                    />
+                ))}
+            </div>   
+            <div className ="title">
+                <h2 className="sub-title">Most Popular Cars in Canada</h2>
+                <Button/>
+                <div className="more-option">View more cities</div>
+            </div>
+            <div className="deal">
+                {cityData.map (cityData=> (
+                <PopularCard 
+                image={cityData.image}
+                cityName={cityData.cityName}
+                model1={cityData.model1}
+                model2={cityData.model2}
+                model3={cityData.model3}
+                />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Cardapp;
