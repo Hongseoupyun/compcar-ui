b4cae82b22d523168077dc164d1e481fa7695391
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-11-18 13:22:40 -0500

    developed(ContactUs):added jsconfig.json for absolute path and wrote unit testing

--- src/components/ContactUs/ContactUs.test.js
+++ src/components/ContactUs/ContactUs.test.js
@@ -1,1 +1,8 @@
 import ContactUs from "./ContactUs";
+import { render } from "@testing-library/react";
+describe("ContactUs", () => {
+  it("renders correctly without crashing", () => {
+    // render the component on virtual dom
+    render(<ContactUs />);
+  });
+});
