8f7580907ade9ace708429cdbfaca09c1dbed4f7
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-11-23 14:47:34 -0500

    developed(ContactUs):Fixed image stretch css

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -4,59 +4,67 @@ import "./ContactUs.scss";
 export default function ContactUs() {
   return (
     <>
-      <div className="contact-us" data-testid = "contact-us-page">
+      <div className="contact-us" data-testid="contact-us-page">
         <div className="meet-our-team">MEET OUR TEAM</div>
         <div className="members-img-container">
-          <div className="members-img-wrapper" data-testid = "img-container">
-            <img
-              id="contact-us-img"
-              alt="sehwan"
-              src={require("assets/Sehwan.jpg")}
-            />
-            <div className="name">
-              <b>Sehwan Oh</b>
-            </div>
-            <div className="role">
-              <i>Lead Developer</i>
-            </div>
-          </div>
-          <div className="members-img-wrapper">
-            <img
-              id="contact-us-img"
-              alt="anna"
-              src={require("assets/anna.jpg")}
-            />
-            <div className="name">
-              <b>Anna Shin</b>
-            </div>
-            <div className="role">
-              <i>FE/UI Developer</i>
+          <div className="members-img-wrapper" data-testid="img-container">
+            <div className="img-cards">
+              <img
+                id="contact-us-img"
+                alt="sehwan"
+                src={require("assets/Sehwan.jpg")}
+              />
+              <div className="name">
+                <b>Sehwan Oh</b>
+              </div>
+              <div className="role">
+                <i>Lead Developer</i>
+              </div>
             </div>
           </div>
           <div className="members-img-wrapper">
-            <img
-              id="contact-us-img"
-              alt="hongseoup"
-              src={require("assets/simon.jpg")}
-            />
-            <div className="name">
-              <b>Simon Yun</b>
-            </div>
-            <div className="role">
-              <i>FE/UI Developer</i>
+            <div className="img-cards">
+              <img
+                id="contact-us-img"
+                alt="anna"
+                src={require("assets/anna.jpg")}
+              />
+              <div className="name">
+                <b>Anna Shin</b>
+              </div>
+              <div className="role">
+                <i>FE/UI Developer</i>
+              </div>
             </div>
           </div>
           <div className="members-img-wrapper">
-            <img
-              id="contact-us-img"
-              alt="jay"
-              src={require("assets/Jay.jpg")}
-            />
-            <div className="name">
-              <b>Jay Choi</b>
+            <div className="img-cards">
+              <img
+                id="contact-us-img"
+                alt="hongseoup"
+                src={require("assets/simon.jpg")}
+              />
+              <div className="name">
+                <b>Simon Yun</b>
+              </div>
+              <div className="role">
+                <i>FE/UI Developer</i>
+              </div>
             </div>
-            <div className="role">
-              <i>BE Developer</i>
+          </div>
+          <div className="members-img-wrapper">
+            <div className="img-cards">
+              <img
+                id="contact-us-img"
+                alt="jay"
+                src={require("assets/Jay.jpg")}
+              />
+              <div className="name">
+                <b>Jay Choi</b>
+              </div>
+              <div className="role">
+                <i>BE Developer</i>
+              </div>
             </div>
           </div>
         </div>
