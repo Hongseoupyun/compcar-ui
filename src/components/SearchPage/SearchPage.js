91a4dfc30d04817e6473fc6d10d7a5d42f40aee9
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-11-25 13:47:09 -0500

    Developed(FET-SearchPage):Added css

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./SearchPage.scss";
-import { HiOutlineMagnifyingGlass } from "react-icons/hi";
+import { FaSearch } from "react-icons/fa";
 import { useState } from "react";
 
 export default function SearchPage() {
@@ -12,28 +12,28 @@ export default function SearchPage() {
       }}
     >
       <h1>FIND YOUR DREAM CAR</h1>
+      <div className="container--search--tap">
+        <div className="tap--search">Search</div>
+        <div className="tap--search" id="compare">Search to compare</div>
+      </div>
       <div className="searchbar--wrapper">
-        <div className="container--search--tap">
-          <div className="search--search">Search</div>
-          <div className="search--compare">Search to compare</div>
-        </div>
         <form className="container--input">
-          <select className="searchbar--make">
+          <select className="searchbar--select">
             <option value="all">Make:All</option>
           </select>
-          <select className="searchbar--model">
+          <select className="searchbar--select">
             <option value="all">Model:All</option>
           </select>
-          <select className="searchbar--year">
+          <select className="searchbar--select">
             <option value="all">Year:All</option>
           </select>
-          <select className="searchbar--color">
+          <select className="searchbar--select">
             <option value="all">Color:All</option>
           </select>
-          <select className="searchbar--mileage">
+          <select className="searchbar--select">
             <option value="all">Mileage:All</option>
           </select>
-          <button className="button--search" type="submit"></button>
+          <button className="button--search" type="submit"><FaSearch id="emoji"/></button>
         </form>
       </div>
     </section>
