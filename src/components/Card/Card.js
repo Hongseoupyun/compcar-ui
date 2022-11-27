ebd34a30a8f6338df76ad232b275c74d58692e8a
ashin526 <anna.shin526@gmail.com>
2022-11-27 18:59:03 -0500

    Card view complete

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,12 +4,12 @@ export default function Card(props) {
   return (
     <div className="contentsCard">
       <div key={props.id} className="carCard">
-        <img src={props.image} alt="car-img" className="carImage"></img>
+        <img src={props.url} alt="car-img" className="carImage"></img>
         <div className="carCard_content">
-          <h3 className="carBrand"> {props.brand}</h3>
+          <h3 className="carBrand"> {props.maker}</h3>
           <div className="carInfo">
-            <div className="model"> Model: {props.model}</div>
-            <div className="price">Price: ${props.price}</div>
+            <div className="model"> Model: {props.car_info}</div>
+            <div className="price">Price: {props.price}</div>
             <div className="mileage">Mileage: {props.mileage} km</div>
           </div>
         </div>
--- src/components/Card/Cardapp.js
+++ src/components/Card/Cardapp.js
@@ -8,16 +8,18 @@ import { useState, useEffect } from "react";
 
 function CardApp() {
   const[recCard, setRecCard] = useState([]);
-
   useEffect(()=>{
     fetch('http://localhost:8000/api/kijijiCar')
     .then((response)=>response.json())
-    .then((data)=>console.log(data))
-    .then((data)=>setRecCard(data));
+    .then((data) => {
+      const newData = data.slice(1, 4);
+      setRecCard(newData);
+    })
+    .then((data)=>console.log(data));
   }, []);
 
   return (
-    <div className="cardApp">
+    <div className="CardApp">
       <div className="title">
         <h2 className="sub-title">The best deals</h2>
         <div className="sub-explain">
@@ -26,11 +28,12 @@ function CardApp() {
         <div className="more-option">see more deals</div>
       </div>
       <div className="deal">
+
         {recCard.map((recData) => (
           <Card
-            image={recData.image}
-            brand={recData.brand}
-            model={recData.model}
+            url={recData.url}
+            maker={recData.maker}
+            car_info={recData.car_info}
             price={recData.price}
             mileage={recData.mileage}
           />
