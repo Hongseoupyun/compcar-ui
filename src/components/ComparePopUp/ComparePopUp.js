8e926a177fde45aca724b5146dbab0e95a3553e6
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-01-05 16:09:12 +0900

    developed(FET-ResultPage):Added compare modal pop up

--- src/components/ComparePopUp/ComparePopUp.js
+++ src/components/ComparePopUp/ComparePopUp.js
@@ -1,14 +1,19 @@
 import React from "react";
-
+import "./ComparePopUp.scss";
 export default function ComparePopUp(props) {
-  const { isComparePopUpOpen } = props;
+  const { isComparePopUpOpen, setIsComparePopUpOpen } = props;
   if (!isComparePopUpOpen) return null;
   return (
     <div className="overlay">
       <div className="modal--container">
         <div className="modal--header">
           <div className="modal--header--heading">Select to compare</div>
-          <div className="modal--header--close">X</div>
+          <div
+            className="modal--header--close"
+            onClick={() => setIsComparePopUpOpen(false)}
+          >
+            X
+          </div>
         </div>
         <div className="modal--title">MY FAVOURITE</div>
         <div className="modal--input">
@@ -17,7 +22,12 @@ export default function ComparePopUp(props) {
           </select>
         </div>
         <div className="modal--button--wrapper">
-          <button className="modal--button--cancel">Cancel</button>
+          <button
+            className="modal--button--cancel"
+            onClick={() => setIsComparePopUpOpen(false)}
+          >
+            Cancel
+          </button>
           <button className="modal--button--select">Select</button>
         </div>
       </div>
